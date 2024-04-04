8acbe0d2e1337751dab8fa9ad95c4057a8887949
root <root@Unaislaptop>
2024-04-04 15:20:07 +0530

    styled the signup and signin with auth auth component that we can reuse

---
+++ frontend/src/components/Auth.tsx
@@ -0,0 +1,95 @@
+import { SignupInput } from "@unaizk/medium-common";
+import { ChangeEvent, useState } from "react";
+import { Link } from "react-router-dom";
+
+const Auth = ({type} : {type : "signup" | "signin"}) => {
+  const [postInputs, setPostInputs] = useState<SignupInput>({
+    email: "",
+    password: "",
+    name: "",
+  });
+  return (
+    <div className="h-screen flex justify-center flex-col">
+      <div className="flex justify-center">
+        <div>
+          <div className="px-10">
+            <div className="text-3xl font-extrabold text-center">{type === 'signin' ? "Log In" : "Create an Account"}</div>
+            <div className="text-slate-400 text-center pt-2">
+              {type === "signin" ? "Dont't have an account" : "Already have an account"}
+              <Link className="pl-2 underline" to={type === "signin" ? "/signup" : "/signin"}>
+                
+                {type === "signin" ? "Sign up" : "Sign in"}
+              </Link>
+            </div>
+          </div>
+          <div className="pt-8">
+            <LabelledInput
+              label="Username"
+              placeholder="Enter Username"
+              onChange={(e) => {
+                setPostInputs({
+                  ...postInputs,
+                  name: e.target.value,
+                });
+              }}
+            />
+            <LabelledInput
+              label="Email"
+              placeholder="Enter Email"
+              onChange={(e) => {
+                setPostInputs({
+                  ...postInputs,
+                  email: e.target.value,
+                });
+              }}
+            />
+            <LabelledInput
+              label="Password"
+              placeholder="Enter Password"
+              type="password"
+              onChange={(e) => {
+                setPostInputs({
+                  ...postInputs,
+                  password: e.target.value,
+                });
+              }}
+            />
+            <button type="button" className=" mt-8 text-white w-full bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">{type === "signup" ? "Sign up" : "Sign in"}</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+interface LabelledInputType {
+  label: string;
+  placeholder: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  type?: string;
+}
+
+function LabelledInput({
+  label,
+  placeholder,
+  onChange,
+  type,
+}: LabelledInputType) {
+  return (
+    <div>
+      <label className="block mb-2 text-sm font-semibold text-black pt-4">
+        {label}
+      </label>
+      <input
+        onChange={onChange}
+        type={type || "text"}
+        id="first_name"
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+        placeholder={placeholder}
+        required
+      />
+    </div>
+  );
+}
+
+export default Auth;
