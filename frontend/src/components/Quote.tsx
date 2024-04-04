a5f1085f2f91219cbf8e8659b41f9b105fa9c03d
root <root@Unaislaptop>
2024-04-04 13:50:00 +0530

    setup frontend and design signup page and done routing

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,25 @@
+
+const Quote = () => {
+  return (
+    <div className="bg-slate-200 h-screen flex justify-center flex-col">
+        <div className="flex justify-center ">
+            <div className="max-w-lg">
+                <div className="   font-bold text-3xl">
+                "The customer service i recieved was exceptional. The support team went above and beyond to address my concerns. "
+                </div>
+                <div className=" max-w-md  font-semibold text-xl mt-4">
+                    Julies Winfield
+                </div>
+                <div className=" max-w-md  text-slate-400 font-light text-sm">
+                    CEO | Acme corp
+                </div>
+            </div>
+           
+        </div>
+        
+       
+    </div>
+  )
+}
+
+export default Quote
---
+++ frontend/src/pages/Signup.tsx
@@ -0,0 +1,15 @@
+import Quote from "../components/Quote"
+
+
+const Signup = () => {
+  return (
+    <div className=" grid grid-cols-2 ">
+        <div>
+            
+        </div>
+        <Quote />
+    </div>
+  )
+}
+
+export default Signup
